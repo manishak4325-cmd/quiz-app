ab6c42fd3beac74bbf3f098449ca0942e39ff263
Manisha Sharma <manisha.k.435@gmail.com>
2026-07-15 08:02:42 +0530

    Debug API URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,13 +1,9 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://quizapp-hzh1.onrender.com/api",
-});
+console.log("API URL =", import.meta.env.VITE_API_URL);
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
